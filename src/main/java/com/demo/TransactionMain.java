551b82a75c602c2fd5e30a1b9993dc5d14405998
Richa <richa@tothenew.com>
2018-03-05 07:37:47 +0530

    Added Skeleton For Transaction Demo

---
+++ src/main/java/com/demo/TransactionMain.java
@@ -0,0 +1,8 @@
+package com.demo;
+
+public class TransactionMain {
+
+    public static void main(String[] args) {
+        // Add Main Method related Code here
+    }
+}
---
+++ src/main/java/com/demo/service/AccountService.java
@@ -0,0 +1,5 @@
+package com.demo.service;
+
+public class AccountService {
+ // Add Transactional Code here
+}
