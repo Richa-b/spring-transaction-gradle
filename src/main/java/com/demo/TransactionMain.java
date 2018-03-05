e9c650a729fdba15541b6e656181d3d125a8ef82
Richa <richa@tothenew.com>
2018-03-05 11:43:13 +0530

    Demos Showing Propagation type

--- src/main/java/com/demo/TransactionMain.java
+++ src/main/java/com/demo/TransactionMain.java
@@ -1,8 +1,18 @@
 package com.demo;
 
+import com.demo.service.AccountService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+
 public class TransactionMain {
 
     public static void main(String[] args) {
         // Add Main Method related Code here
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(TransactionConfig.class);
+        AccountService accountService =(AccountService) applicationContext.getBean("accountService");
+        accountService.insertEmployee();
     }
 }
--- src/main/java/com/demo/service/AccountService.java
+++ src/main/java/com/demo/service/AccountService.java
@@ -1,5 +1,53 @@
 package com.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+
+@Component
 public class AccountService {
- // Add Transactional Code here
+
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+
+    @Autowired
+    PlatformTransactionManager transactionManager;
+
+    @Autowired
+    LoggingService loggingService;
+
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void insertEmployee() {
+        System.out.println("Inserting into table");
+        String sql = "INSERT INTO user_account (username,amount)VALUES(?,?)";
+        jdbcTemplate.update(sql, new Object[]{"Peter", 2700});
+        try {
+            loggingService.loggingIntoTransactionTable();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+   /* @Transactional(propagation = Propagation.REQUIRED)
+    public void insertEmployee1() throws Exception {
+        System.out.println("Inserting into table");
+        String sql = "INSERT INTO user_account (username,amount)VALUES(?,?)";
+        jdbcTemplate.update(sql, new Object[]{"Peter", 2700});
+        for (int i = 0; i < 3; i++) {
+            loggingService.loggingIntoTransactionTable1();
+        }
+        throw new Exception();
+
+    }*/
 }
